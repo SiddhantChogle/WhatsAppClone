7c61b50438ab036d09b2113f5de78ce799078443
Siddhant Chogle <siddhantchogle7@gmail.com>
2020-10-13 21:42:30 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/whatsapp_home.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "WhatsAppClone",
+      theme: ThemeData(
+        primaryColor: Color(0xff075E54),
+        accentColor: Color(0xff25D366),
+      ),
+      home: WhatsAppHome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
